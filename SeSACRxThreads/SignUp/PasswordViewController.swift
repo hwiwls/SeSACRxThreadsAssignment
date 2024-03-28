ae3452666372ec496cc91c64eded63149b81b237
hwiwls <80039139+hwiwls@users.noreply.github.com>
2024-03-29 02:05:28 +0900

    feat: 비밀번호 유효성 검사 로직

--- SeSACRxThreads/SignUp/PasswordViewController.swift
+++ SeSACRxThreads/SignUp/PasswordViewController.swift
@@ -7,11 +7,19 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class PasswordViewController: UIViewController {
    
     let passwordTextField = SignTextField(placeholderText: "비밀번호를 입력해주세요")
     let nextButton = PointButton(title: "다음")
+    
+    let descriptionLabel = UILabel()
+    
+    let validText = Observable.just("8자 이상 입력해주세요")
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,21 +27,57 @@ class PasswordViewController: UIViewController {
         view.backgroundColor = Color.white
         
         configureLayout()
-         
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        
+        bind()
     }
     
-    @objc func nextButtonClicked() {
-        navigationController?.pushViewController(PhoneViewController(), animated: true)
+    func bind() {
+        validText
+            .bind(to: descriptionLabel.rx.text)
+            .disposed(by: disposeBag)   // validText Observable이 descriptionLabel의 text 속성에 바인딩
+        
+        let validation = passwordTextField
+            .rx
+            .text
+            .orEmpty
+            .map { $0.count >= 8 }  // 비밀번호 텍스트 필드의 텍스트를 관찰하고, 입력된 텍스트가 8자 이상인지 여부에 따라 Bool 값을 방출하는 validation Observable
+        
+        validation
+            .bind(to: nextButton.rx.isEnabled, descriptionLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        validation
+            .bind(with: self) { owner, value in // owner: (self)PasswordViewController, value: 비밀번호가 8자 이상일 경우 true, 그렇지 않을 경우 false
+                let color: UIColor = value ? .systemPink : .lightGray
+                owner.nextButton.backgroundColor = color
+            }
+            .disposed(by: disposeBag)
+        
+        nextButton
+            .rx
+            .tap
+            .bind(with: self) { owner, _ in
+                print("show alert")
+            }
+            .disposed(by: disposeBag)
+        
     }
+    
     
     func configureLayout() {
         view.addSubview(passwordTextField)
+        view.addSubview(descriptionLabel)
         view.addSubview(nextButton)
          
         passwordTextField.snp.makeConstraints { make in
             make.height.equalTo(50)
             make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.height.equalTo(20)
+            make.top.equalTo(passwordTextField.snp.bottom)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
         
